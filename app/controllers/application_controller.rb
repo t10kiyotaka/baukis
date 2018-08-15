e36cc6ba06e160c7e8f2d071f8b37d3fd6f7cc11
冨井聖敬 <>
2018-08-15 16:27:29 +0900

    Make top page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :set_layout
+  def set_layout
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      'customer'
+    end
+  end
 end
