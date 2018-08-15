23ff0f623268b037e5e3bd3c0f5afbba612eddaa
冨井聖敬 <>
2018-08-16 08:04:05 +0900

    Make error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   layout :set_layout
+
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers if Rails.env.production?
+
+  private
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
@@ -11,4 +18,5 @@ class ApplicationController < ActionController::Base
       'customer'
     end
   end
+
 end
