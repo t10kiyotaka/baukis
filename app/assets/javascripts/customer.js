8d7700bc3b2b3ac56a5c6b34eec85517bcc13790
冨井聖敬 <>
2018-09-17 16:30:18 +0900

    Add datepicker in staff_members/edit

--- app/assets/javascripts/customer.js
+++ app/assets/javascripts/customer.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.ui.datepicker
+//= require jquery.ui.datepicker-jaº
 //= require_tree ./shared
 //= require_tree ./customer
