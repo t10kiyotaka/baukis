b1636bcd714754f761af21089a01c74e617e1d4a
冨井聖敬 <t10kiyotaka@gmail.com>
2018-09-22 20:06:16 +0900

    Add staff/customers controller

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,7 @@ class Customer < ActiveRecord::Base
 
   def password=(raw_password)
     if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::password.create(raw_password)
+      self.hashed_password = BCrypt::Password.create(raw_password)
     elsif raw_password.nil?
       self.hashed_password = nil
     end
